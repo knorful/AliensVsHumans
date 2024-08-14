56bc02e55bc3abef705320e421f8df8aa7947393
Kris Norful <krisnorful@gmail.com>
2024-08-14 06:03:05 -0500

    test functionality

--- src/main/java/com/example/AliensVsHumans/AliensVsHumansApplication.java
+++ src/main/java/com/example/AliensVsHumans/AliensVsHumansApplication.java
@@ -1,5 +1,7 @@
 package com.example.AliensVsHumans;
 
+import models.Alien;
+import models.AlienRace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AliensVsHumansApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AliensVsHumansApplication.class, args);
+//		SpringApplication.run(AliensVsHumansApplication.class, args);
+		Alien martian = new Alien(AlienRace.MARTIAN);
+
+		System.out.println("Initial health: " + martian.getCurrentHealth());
+		martian.takeDamage(5);
+		System.out.println("After taking damage: " + martian.getCurrentHealth());
+
+		System.out.println(martian.regenerateHealth(15, 55)); // Regenerates 15 health points
 	}
 
 }
