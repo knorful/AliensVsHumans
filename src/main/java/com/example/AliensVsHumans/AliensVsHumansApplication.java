5a81acf4b2d916b624129a3d159f87f984faab72
Kris Norful <krisnorful@gmail.com>
2024-08-15 05:23:46 -0500

    small test to see if basic logic works

--- src/main/java/com/example/AliensVsHumans/AliensVsHumansApplication.java
+++ src/main/java/com/example/AliensVsHumans/AliensVsHumansApplication.java
@@ -14,11 +14,18 @@ public class AliensVsHumansApplication {
 		Alien martian = new Alien(AlienRace.MARTIAN);
 		Human human = new Human("Boog", 50, true, 100);
 
-		int damage = martian.dealDamage();
+		int alienDamageDealt = martian.dealDamage();
 		System.out.println("Initial health: " + human.getCurrentHealth());
-		System.out.println("Alien does " + damage + " damage to " + human.getName());
-		human.takeDamage(damage);
+		System.out.println("Alien does " + alienDamageDealt + " damage to " + human.getName());
+		human.takeDamage(alienDamageDealt);
 		System.out.println("After taking damage, "+ human.getName() + " has " + human.getCurrentHealth() + " health");
+		System.out.println("-------- Human turn to attack ---------");
+
+		int humanDamageDealt = human.dealDamage();
+		System.out.println("Initial health: " + martian.getCurrentHealth());
+		System.out.println(human.getName() + " does " + humanDamageDealt + " damage to alien!");
+		martian.takeDamage(humanDamageDealt);
+		System.out.println("After taking damage, the alien has " + martian.getCurrentHealth() + " health");
 
 //		System.out.println(martian.regenerateHealth(15, 55)); // Regenerates 15 health points
 	}
